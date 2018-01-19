440196a5fc5c42098843efa217c1816c0d99b785
Naookie Sato <necrosato@live.com>
2018-01-19 15:41:58 -0800

    db works

--- users_functions.py
+++ users_functions.py
@@ -20,3 +20,12 @@ def user_change_password(dbcursor, user, password):
     query = "UPDATE users SET password = PASSWORD(%s) WHERE email = %s"
     dbcursor.execute(query, (password,user,))
 
+def user_create(dbcursor, name, email, password, phone=None):
+    query = "INSERT INTO users (name, email, password) VALUES (%s, %s, PASSWORD(%s))"
+    query_phone = "INSERT INTO users (name, email, phone, password) VALUES (%s, %s, %s, PASSWORD(%s))"
+    dbcursor.execute(query, (name, email, password,)) if phone == None else dbcursor.execute(query_phone, (name, email, phone, password))
+
+def user_delete(dbcursor, name, email, password):
+    query = "DELETE FROM users WHERE name = %s AND email = %s AND password = PASSWORD(%s)"
+    dbcursor.execute(query, (name, email, password,))
+
