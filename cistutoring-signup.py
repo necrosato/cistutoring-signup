9ca7e81dc88b53d5f9715409978761821d6ecb4f
Naookie Sato <necrosato@live.com>
2018-01-22 22:59:38 -0800

    got session variables working

--- cistutoring-signup.py
+++ cistutoring-signup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, session, redirect, escape, request, url_for
 from flaskext.mysql import MySQL
-from flask_table import Table, Col
 from datetime import datetime, timedelta
 
 from users_functions import *
 from events_functions import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY']=os.urandom(24)
 mysql = MySQL()
 
 #mysql config
@@ -21,50 +22,37 @@ mysql.init_app(app)
 def signin():
     return render_template('signin.html')
 
-display_weeknum = 0
 @app.route("/")
 @app.route("/schedule")
 def schedule():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    tdelt = timedelta(weeks=display_weeknum)
-    dt = datetime.today()+tdelt
-    days = get_week_events(cursor, dt)
-    conn.close()
-
-    ws = week_begin(dt).strftime('%Y-%m-%d')
-    we = week_end(dt).strftime('%Y-%m-%d')
-    return render_template('schedule.html',days=days[1:6], week_start=ws, week_end=we)
+    if 'display_weeknum' in session:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        tdelt = timedelta(weeks=session['display_weeknum'])
+        dt = datetime.today()+tdelt
+        days = get_week_events(cursor, dt)
+        conn.close()
+        ws = week_begin(dt).strftime('%Y-%m-%d')
+        we = week_end(dt).strftime('%Y-%m-%d')
+        return render_template('schedule.html',days=days[1:6], week_start=ws, week_end=we)
+    else:
+        session['display_weeknum']=0
+        return schedule()
 
 @app.route("/week-next")
 def schedule_week_next():
-    global display_weeknum
-    display_weeknum+=1
-    return schedule()
+    if 'display_weeknum' in session:
+        session['display_weeknum']+=1
+    return redirect(url_for('schedule'))
 @app.route("/week-prev")
 def schedule_week_prev():
-    global display_weeknum
-    display_weeknum-=1
-    return schedule()
+    if 'display_weeknum' in session:
+        session['display_weeknum']-=1
+    return redirect(url_for('schedule'))
 
 @app.route("/signup")
 def signup():
     return render_template('signup.html')
- 
-
-@app.route("/sqltest")
-def sqltest():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-
-    #events = datetime_range_strings(2018, 1, 23, 18, 0, 19, 0, 1)
-    #event_reserve_range(cursor, 1, events)
-    #days = get_week_events(cursor, datetime.today()+timedelta(weeks=1))
-
-    conn.commit() # this is important to save changes to the db, must include
-    conn.close()
-    return schedule()
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000)
